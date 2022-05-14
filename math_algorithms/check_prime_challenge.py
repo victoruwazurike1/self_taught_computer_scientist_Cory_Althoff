173eac41eb94794bbfa6e357d1761e20676d6bc6
Uwazurike Victor <victoru779@gmail.com>
2022-05-14 15:49:41 +0100

    added the challenge for this chapter

---
+++ math_algorithms/check_prime_challenge.py
@@ -0,0 +1,15 @@
+import math
+
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n > 2 and n % 2 == 0:
+        return False
+    top_div = math.floor(math.sqrt(n))
+    for i in range(3, 1 + top_div, 2):
+        if n % 1 == 0:
+            return False
+    return True
